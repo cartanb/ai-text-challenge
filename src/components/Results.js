4ce2efa64b16865c0e4425ebd37a02c66beeb371
Brian Cartan <cartanb@gmail.com>
2022-05-18 00:17:48 -0400

    Successful POST requests to OpenAI

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,13 +1,16 @@
-function Results() {
+function Results(props) {
   return (
-    <output htmlFor="prompt.">
+    <output htmlFor="prompt">
       <h3>Responses:</h3>
       <ul>
-        {/* If results array is empty, render: */}
-        <li>Nothing submitted yet! Why not send a prompt?</li>
+        {props.completions.length ? (
+          <h3>RESPONSES PLACEHOLDER</h3>
+        ) : (
+          <li>Nothing submitted yet! Why not send a prompt?</li>
+        )}
       </ul>
     </output>
-  )
+  );
 }
 
 export default Results;
