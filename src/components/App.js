96c1522aa2baf67c82392f8ce23a9371cbfea933
Brian Cartan <cartanb@gmail.com>
2022-05-18 09:30:02 -0400

    Displays responses to sent prompts

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Fun with AI</h1>
-        <h2>Shopify Front End Dev Intern Challenge</h2>
+        <h2>Front End Dev Intern Challenge</h2>
       </header>
       <main>
         <Prompt />
--- src/components/Results.js
+++ src/components/Results.js
@@ -4,7 +4,14 @@ function Results(props) {
       <h3>Responses:</h3>
       <ul>
         {props.completions.length ? (
-          <h3>RESPONSES PLACEHOLDER</h3>
+          props.completions.map((completion, idx) => (
+            <li key={idx}>
+              <span>Prompt:</span>
+              <p id={`prompt${idx}`}>{completion[0]}</p>
+              <span>Response:</span>
+              <p id={`comp${idx}`}>{completion[1]}</p>
+            </li>
+          ))
         ) : (
           <li>Nothing submitted yet! Why not send a prompt?</li>
         )}
